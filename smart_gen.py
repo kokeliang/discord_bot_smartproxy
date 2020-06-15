0aef7141dfbd273ed18406f1b2996e9a623ea45d
koke liang <44743675+kokeliang@users.noreply.github.com>
2020-06-15 11:02:09 +0200

    Update and rename smart_gen_2.py to smart_gen.py

--- smart_gen.py
+++ smart_gen.py
@@ -4,11 +4,12 @@ import random
 
 
 class SmartBot:
-    def __init__(self, region, acc, number):
+    # input your account and password for smart proxy, the number of proxies you want to generate and which country od region of proxies you want.
+    def __init__(self, region, account, number):
         self.region = region
-        self.acc = acc
+        self.acc = account
         self.number = number
-
+    # find out how many ports and the port range are in the region or country form smart proxy csv file
     def proxy_gen(self):
         file = pd.read_csv('smart_sticky.csv')
         quantity_all = []
@@ -19,7 +20,7 @@ class SmartBot:
             quantity_range.append([int(m.groups()[0]), int(m.groups()[1])])
             quantity = int(m.groups()[1]) - int(m.groups()[0])
             quantity_all.append(quantity)
-
+    # generate the proxies with proportion of port quantity.
         quantity_sum = sum(quantity_all)
         i = 0
         random_proxy = []
@@ -31,6 +32,6 @@ class SmartBot:
                               + ':' + self.acc
                 random_proxy.append(random_port + '\n')
             i += 1
-
+    # write down the proxies file
         with open(r'./proxy.txt', 'w') as f:
             f.write(''.join(random.sample(random_proxy, len(random_proxy))))
